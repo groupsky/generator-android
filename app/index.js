9a94c1617ae8af27e2837ba5de9deb7bf84a9abb
Geno Roupsky <geno@roupsky.name>
2014-01-16 12:15:17 +0200

    created basic questions

--- app/index.js
+++ app/index.js
@@ -2,6 +2,28 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+var androidSDKversions = [
+    'API 1',
+    'API 2',
+    'API 3',
+    'API 4',
+    'API 5',
+    'API 6',
+    'API 7',
+    'API 8: Android 2.2 (Froyo)', 
+    'API 9: Android 2.3 (Gingerbread)', 
+    'API 10: Android 2.3.3 (Gingerbread)', 
+    'API 11',
+    'API 12',
+    'API 13',
+    'API 14: Android 4.0 (Ice Cream Sandwich)', 
+    'API 15: Android 4.0.3 (Ice Cream Sandwich)',
+    'API 16: Android 4.1 (Jelly Bean)',
+    'API 17: Android 4.2 (Jelly Bean)',
+    'API 18: Android 4.3 (Jelly Bean)',
+    'API 19: Android 4.4.2 (KitKat'].map(function(name, value) {
+      return {name: name, value: value};
+    });
 
 
 var AndroidGenerator = module.exports = function AndroidGenerator(args, options, config) {
@@ -22,15 +44,51 @@ AndroidGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+  var prompts = [
+  {
+    name: 'applicationName',
+    message: 'What is the name of your application?',
+    default: 'My Application'
+  },
+  {
+    name: 'projectName',
+    message: 'How do you want to call your project?',
+    default: function(props) {
+      return props.applicationName.replace(/ /g,'');
+    }
+  },
+  {
+    name: 'packageName',
+    message: 'What is the package name?',
+    default: 'com.example.app'
+  },
+  {
+    name: 'minimumApiLevel',
+    message: 'Minimum required SDK:',
+    type: 'list',
+    choices: androidSDKversions,
+    default: 8
+  },
+  {
+    name: 'targetApiLevel',
+    message: 'Target SDK:',
+    type: 'list',
+    choices: androidSDKversions,
+    default: androidSDKversions.length-1
+  },
+  {
+    name: 'compileApiLevel',
+    message: 'Compile with:',
+    type: 'list',
+    choices: androidSDKversions,
+    default: function(props) {
+      console.log("props = "+JSON.stringify(props));
+      return props.targetApiLevel;
+    }
   }];
 
   this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+    this.props = props;
 
     cb();
   }.bind(this));
